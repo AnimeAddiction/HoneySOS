a868fa0e1e0cb57939c8e65938f143257377b39a
Poop <70053528+AnimeAddiction@users.noreply.github.com>
2024-04-27 12:42:35 +0800

    Correct splashpage gif added

--- WindowsFormsApp1/Properties/Resources.Designer.cs
+++ WindowsFormsApp1/Properties/Resources.Designer.cs
@@ -79,5 +79,15 @@ namespace WindowsFormsApp1.Properties {
                 return ((System.Drawing.Bitmap)(obj));
             }
         }
+        
+        /// <summary>
+        ///   Looks up a localized resource of type System.Drawing.Bitmap.
+        /// </summary>
+        internal static System.Drawing.Bitmap Startup_ver_4 {
+            get {
+                object obj = ResourceManager.GetObject("Startup ver 4", resourceCulture);
+                return ((System.Drawing.Bitmap)(obj));
+            }
+        }
     }
 }
